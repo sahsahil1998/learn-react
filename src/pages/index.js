85632da2cc076246beea82513aa7d99de849bc21
sahsahil1998 <sahsahil1997@gmail.com>
2023-10-19 20:16:16 -0400

    list_keys

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile.js'
+import List from './components/list_keys'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <List />
     </div>
   )
 }
